b682b0c361e44236632b708c33d729774143fc7a
Marina <m.sapezhinskaya@gmail.com>
2024-02-08 19:40:29 +0200

    Initial commit

---
+++ src/main/java/com/app/sequrity/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.app.sequrity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+       @Bean
+        public BCryptPasswordEncoder passwordEncoder() {
+           return new BCryptPasswordEncoder();
+       }
+
+       @Bean
+        public UserDetailsService userDetailsService() {
+           return new CustomUserDetailsService();
+       }
+
+       @Bean
+        public DaoAuthenticationProvider authenticationProvider() {
+           DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+           provider.setPasswordEncoder(passwordEncoder());
+           provider.setUserDetailsService(userDetailsService());
+
+           return provider;
+       }
+
+       @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+           http.csrf().disable().authorizeRequests()
+
+       }
+}
